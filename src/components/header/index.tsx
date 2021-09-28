2c3b98da226849842505ef51e4a1a5c96b55dcfb
jsimpson15011 <joseph.franklin.simpson@gmail.com>
2021-09-27 21:25:07 -0600

    Add header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,50 @@
 import React from "react";
+import Link from "next/link"
 
 import { Logo } from "@components";
 
+import styles from "./header.module.css"
+
 export const Header: React.FC = () => {
   return (
-    <div className="text-center bg-gray-800"  >
-      <Logo />
+    <div className={styles.header}  >
+        <div className={styles.nav}>
+            <div className={styles.navWrapper}>
+                <div className={styles.navRow}>
+                    <Link href="/">
+                        <a>Home</a>
+                    </Link>
+                    <Link href="/about">
+                        <a>About</a>
+                    </Link>
+                    <Link href="/projects">
+                        <a>Projects</a>
+                    </Link>
+                </div>
+                <Link href="/">
+                    <a>
+                        <img alt="" src="/images/js-logo.png"/>
+                    </a>
+                </Link>
+                <div className={styles.navRow}>
+                    <Link href="/">
+                        <a>Home</a>
+                    </Link>
+                    <Link href="/about">
+                        <a>About</a>
+                    </Link>
+                    <Link href="/projects">
+                        <a>Projects</a>
+                    </Link>
+                </div>
+            </div>
+        </div>
+        <h1 className={styles.title}>
+            Joseph Simpson
+            <span className={styles.subTitle}>
+                Full Stack Developer
+            </span>
+        </h1>
     </div>
   );
 };
